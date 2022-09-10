26fedf61458ef7146a1157210f253702557b203c
Victor Røer <viggo00@hotmail.com>
2022-09-10 17:23:38 +0200

    Add counter

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-Sortable.create(simpleList, { 
-    group: {
-        name: 'shared',
-        put: function (to) {
-            return to.el.children.length < 15;
-        }
-    }  
+var limit = 15;
+
+function updateLimit(amount) {
+	var counterElement = document.getElementById("counter");
+	counterElement.innerHTML = `${amount}/${limit}`;
+}
+
+Sortable.create(simpleList, {
+	group: {
+		name: 'shared',
+		put: function (to) {
+			return to.el.children.length < limit;
+		}
+	},
+	onSort: function () {
+		updateLimit(this.el.children.length);
+	}
 });
 
-Sortable.create(simpleList2, { 
-    group: 'shared',
- });
+Sortable.create(simpleList2, {
+	group: 'shared',
+});
+
+updateLimit(simpleList.children.length);
